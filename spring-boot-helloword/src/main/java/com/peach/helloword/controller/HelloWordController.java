baddaa0fd18cc415875f82b11a2d7848a660441b
peach <49863818@qq.com>
2019-06-26 14:37:40 +0800

    Spring Boot  helloword

---
+++ spring-boot-helloword/src/main/java/com/peach/helloword/controller/HelloWordController.java
@@ -0,0 +1,24 @@
+package com.peach.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author 圈哥
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/6/26
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloWordController {
+
+    @GetMapping("/say")
+    public  String say(){
+        return  "hello word Spring Boot";
+    }
+
+
+}
