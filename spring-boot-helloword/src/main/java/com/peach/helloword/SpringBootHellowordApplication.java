aa53861fcb3a4538dfb5bca3d58a7ad593227413
peach <49863818@qq.com>
2019-06-26 14:20:29 +0800

    Spring Boot  helloword

---
+++ spring-boot-helloword/src/main/java/com/peach/helloword/SpringBootHellowordApplication.java
@@ -0,0 +1,13 @@
+package com.peach.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHellowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHellowordApplication.class, args);
+    }
+
+}
