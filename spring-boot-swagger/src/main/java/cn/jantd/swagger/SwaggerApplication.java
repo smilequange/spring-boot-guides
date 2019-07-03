378feb71403976718ec2b5e02ae6b831b95e09f9
peach <49863818@qq.com>
2019-07-03 19:21:51 +0800

    spring-boot-swagger

---
+++ spring-boot-swagger/src/main/java/cn/jantd/swagger/SwaggerApplication.java
@@ -0,0 +1,35 @@
+package cn.jantd.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @Description
+ * @Author  圈哥
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2019/7/3
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+
+    public static void main(String[] args)  throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(SwaggerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        System.out.printf("\n----------------------------------------------------------\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
+                "----------------------------------------------------------");
+    }
+
+}
