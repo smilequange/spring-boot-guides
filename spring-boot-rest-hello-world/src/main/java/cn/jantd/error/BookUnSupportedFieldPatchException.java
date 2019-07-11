c181496d5fa266229f751875d470adc847bf1b1c
peach <49863818@qq.com>
2019-07-11 10:26:03 +0800

    rest

---
+++ spring-boot-rest-hello-world/src/main/java/cn/jantd/error/BookUnSupportedFieldPatchException.java
@@ -0,0 +1,11 @@
+package cn.jantd.error;
+
+import java.util.Set;
+
+public class BookUnSupportedFieldPatchException extends RuntimeException {
+
+    public BookUnSupportedFieldPatchException(Set<String> keys) {
+        super("Field " + keys.toString() + " update is not allow.");
+    }
+
+}
