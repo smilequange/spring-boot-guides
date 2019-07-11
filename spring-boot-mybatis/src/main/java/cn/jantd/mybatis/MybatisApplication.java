631d869e2f4b52d408eb969aaad8632f9491cfaf
peach <49863818@qq.com>
2019-07-11 10:26:49 +0800

    rest

--- spring-boot-mybatis/src/main/java/cn/jantd/mybatis/MybatisApplication.java
+++ spring-boot-mybatis/src/main/java/cn/jantd/mybatis/MybatisApplication.java
@@ -1,9 +1,11 @@
 package cn.jantd.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.jantd.mybatis.mapper")
 public class MybatisApplication {
 
     public static void main(String[] args) {
--- spring-boot-mybatis/src/test/java/cn/jantd/mybatis/mapper/UserMapperTest.java
+++ spring-boot-mybatis/src/test/java/cn/jantd/mybatis/mapper/UserMapperTest.java
@@ -1,82 +1,110 @@
 package cn.jantd.mybatis.mapper;
 
 import cn.jantd.mybatis.model.User;
+import cn.jantd.mybatis.model.UserCriteria;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class UserMapperTest {
     @Autowired
     private  UserMapper userMapper ;
 
     @Before
     public void setUp() throws Exception {
-        int[] ages = {10,15,20,25,30};
-        String[] userNames={"张三","李四","王五","老大","老二"};
-        User user ;
-        for (int i = 0; i < ages.length; i++) {
-            //插入基础数据
-             user = new User();
-            user.setAge(ages[i]);
-            user.setUserName(userNames[i]);
-
-
-        }
-
-
     }
 
     @After
     public void tearDown() throws Exception {
     }
 
+    /**
+     * 插入一条测试数据
+     */
+    private  User  insertUserDate(){
+        User user = new User();
+        user.setUserName("quange");
+        user.setAge(18);
+        userMapper.insert(user);
+        return  user;
+    }
+
     @Test
     public void countByExample() {
+        User user = new User();
+        user.setUserName("quange");
+        user.setAge(18);
+        int resultCode =  userMapper.insert(user);
+        Assert.assertEquals(1,resultCode);
+        UserCriteria userCriteria = new UserCriteria();
+        userCriteria.createCriteria().andUserNameEqualTo("quange");
+        long count = userMapper.countByExample(userCriteria);
+        Assert.assertEquals(1,count);
     }
 
     @Test
     public void deleteByExample() {
-    }
-
-    @Test
-    public void deleteByPrimaryKey() {
+        insertUserDate();
+        UserCriteria userCriteria = new UserCriteria();
+        userCriteria.createCriteria().andUserNameEqualTo("quange");
+        int resultCode =  userMapper.deleteByExample(userCriteria);
+        Assert.assertEquals(1,resultCode);
     }
 
     @Test
     public void insert() {
+        User user = new User();
+        user.setUserName("quange");
+        user.setAge(18);
+        int resultCode =  userMapper.insert(user);
+        Assert.assertEquals(1,resultCode);
     }
 
     @Test
+    public void deleteByPrimaryKey() {
+        insertUserDate();
+        UserCriteria userCriteria = new UserCriteria();
+        userCriteria.createCriteria().andUserNameEqualTo("quange");
+        List<User> users = userMapper.selectByExample(userCriteria);
+        Assert.assertEquals(1,userMapper.deleteByPrimaryKey(users.get(0).getUserId()));
+    }
+
+
+
+    @Test
     public void insertSelective() {
+        User user = new User();
+        user.setUserName("quange");
+        int resultCode =  userMapper.insertSelective(user);
+        Assert.assertEquals(1,resultCode);
     }
 
     @Test
     public void selectByExample() {
+        insertUserDate();
+        UserCriteria userCriteria = new UserCriteria();
+        userCriteria.createCriteria().andUserNameEqualTo("quange");
+        List<User> users = userMapper.selectByExample(userCriteria);
+        Assert.assertEquals("quange", users.get(0).getUserName());
+        Assert.assertEquals(18, users.get(0).getAge().intValue());
     }
 
     @Test
     public void selectByPrimaryKey() {
+        User user = insertUserDate();
+        User actualUser = userMapper.selectByPrimaryKey(user.getUserId());
+        Assert.assertEquals(user.getUserId(),actualUser.getUserId());
     }
 
-    @Test
-    public void updateByExampleSelective() {
-    }
-
-    @Test
-    public void updateByExample() {
-    }
-
-    @Test
-    public void updateByPrimaryKeySelective() {
-    }
-
-    @Test
-    public void updateByPrimaryKey() {
-    }
 }
