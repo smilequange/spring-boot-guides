1b23ce74218a938e7bdde869a9f01faad0c77717
peach <49863818@qq.com>
2019-07-12 23:10:21 +0800

    spring-boot-mybatis

---
+++ spring-boot-mybatisplus/src/main/java/cn/jantd/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package cn.jantd.mybatisplus.mapper;
+
+
+import cn.jantd.mybatisplus.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ spring-boot-mybatisplus/src/main/resources/db/data-h2.sql
@@ -0,0 +1,8 @@
+DELETE FROM user;
+
+INSERT INTO user (id, name, age, email) VALUES
+(1, 'leo', 18, 'test@163.com'),
+(2, 'keep', 36, 'test@163.com'),
+(3, 'pure', 28, 'test@163.com'),
+(4, 'smile', 21, 'test@163.com'),
+(5, 'it', 24, 'test@163.com');
