2da60182369c8a81bfb0588dc36ce521b06a9a1b
peach <49863818@qq.com>
2019-07-12 21:44:43 +0800

    spring-boot-jwt

--- spring-boot-jwt/src/main/java/cn/jantd/jwt/controller/TestController.java
+++ spring-boot-jwt/src/main/java/cn/jantd/jwt/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.jantd.jwt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
 
   @ResponseBody
-  @RequestMapping("hello")
+  @GetMapping("hello")
   public Object hello() {
     return "hello world";
   }
